c9b467da7d7fd8cc61a825b9da0bbcfa43f74b85
Mark Hester <marky360@live.ie>
2025-10-16 16:20:32 +0100

    tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,17 +3,8 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
-use Tests\Concerns\MocksSshConnections;
 
 abstract class TestCase extends BaseTestCase
 {
-    use MocksSshConnections;
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        // Always mock SSH connections to prevent actual remote connections during tests
-        $this->mockSuccessfulSshConnections(['mock ssh output']);
-    }
+    //
 }
